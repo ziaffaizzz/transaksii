6406ddc484a3821f78ae263461b27d7ea385811c
ziaffaizzz <kylesmile067@gmail.com>
2026-08-10 21:13:12 +0700

    Add files via upload

---
+++ master/application/config/config.php
@@ -0,0 +1,9 @@
+<?php
+// * url default localhost
+define("BASEURL", "http://localhost/transaksi/");
+
+// * informasi database 
+define("DB_HOST", "localhost"); // * host server 
+define("DB_USER", "root"); // * user database 
+define("DB_PASS", ""); // * password database 
+define("DB_NAME", "transaksibase"); // * nama database
